9560b43d9fc611662db94d76a876c27aa1157082
Nick Sette <njs2394@gmail.com>
2023-11-22 18:29:24 -0500

    shopping list

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [itemState, setItemState] = useState(true)
+
+  const itemClass = itemState ? "" : "in-cart"
+  const buttonObj = itemState ?
+    { buttonClass: "add", buttonName: "Add to Cart" }
+    :
+    { buttonClass: "remove", buttonName: "Remove from Cart" }
+  function listHandler() {
+    setItemState((shelf) => !shelf)
+  }
+
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={listHandler}
+        className={buttonObj.buttonClass}>{buttonObj.buttonName}</button>
     </li>
   );
 }
